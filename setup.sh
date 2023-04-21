9c84694c38180dffa88bcf4e5b5852ca2a7496cf
cryonayes <cryonayes@gmail.com>
2023-04-21 17:31:10 +0300

    Fix

--- setup.sh
+++ setup.sh
@@ -26,6 +26,9 @@ sudo apt install -y gnome-tweaks dconf-editor sassc gettext;
 git clone https://github.com/G-dH/custom-hot-corners-extended.git /tmp/CHC;
 cd /tmp/CHC && make install;
 
+git clone https://github.com/aunetx/blur-my-shell /tmp/BMS;
+cd /tmp/BMS && make install;
+
 # Themes
 git clone https://github.com/vinceliuice/Tela-icon-theme.git /tmp/Tela_RED && bash /tmp/Tela_RED/install.sh red;
 gsettings set org.gnome.desktop.interface icon-theme 'Tela-red-dark';
@@ -37,15 +40,10 @@ git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git /tmp/Whitesur_GT
 gsettings set org.gnome.shell.extensions.user-theme name 'WhiteSur-Dark-red';
 
 wget https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.3/Bibata-Modern-Ice.tar.gz -O /tmp/Bibata.tar.gz;
-tar -xvf /tmp/Bibata.tar.gz && mv Bibata-* ~/.icons;
-gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice';
+mkdir -p ~/.icons && tar -xvf /tmp/Bibata.tar.gz -C ~/.icons && gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice';
 
 # JetBrains Mono Font
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
-
-# Generate ssh-key for github
-echo "$HOME/.ssh/github" | ssh-keygen -t ed25519 -C "cryonayes@gmail.com";
-echo -e "SSH KEY FOR GITHUB:" && cat ~/.ssh/github.pub;
 
 # Shortcuts
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "[]"
@@ -53,16 +51,23 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Control><Alt>Left']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Control><Alt>Right']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Shift><Alt>Left']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Shift><Alt>Right']"
 gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
 gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-center "['<Super>C']"
-
-# Git setup
-git config --global init.defaultBranch main
-# git config --global user.name ""
-# git config --global user.email ""
 
 # OhMyZsh installation
 sudo apt install -y zsh;
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Delete temporary files
+rm -rf $DOWNLOAD_DIR /tmp/CHC /tmp/BMS /tmp/Tela_RED /tmp/Colloid_GTK /tmp/Whitesur_GTK /tmp/Bibata.tar.gz 
+
+# Git setup
+git config --global init.defaultBranch main
+git config --global user.name "username"
+git config --global user.email "mail@gmail.com"
+
+# Generate ssh-key for github
+echo "$HOME/.ssh/github" | ssh-keygen -t ed25519 -C "mail@gmail.com";
+echo -e "SSH KEY FOR GITHUB:" && cat ~/.ssh/github.pub;
